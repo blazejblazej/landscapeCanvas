0d112394cad36eb7c7d11757d868dc17f374d429
SaidCosmin <cosminsaid@gmail.com>
2021-03-05 21:23:15 +0200

    add initial structure

---
+++ script.js
@@ -0,0 +1,20 @@
+function draw() {
+  const canvas = document.getElementById("can1");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    // draw sky & clounds
+
+    // draw birds
+
+    // draw grass
+
+    // draw rainbow
+
+    // draw trees
+
+    // draw flowers
+  }
+}
+// event listener for the function to "draw" the illustration when the page has loaded
+window.addEventListener("load", draw);
