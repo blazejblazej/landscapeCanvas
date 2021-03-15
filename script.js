0aab8f9d2f41e65baea6c0d44fa12df3bd110ca8
blazej <blazejplis@gmail.com>
2021-03-15 19:15:26 +0100

    Added drawBird funciton and draw 3 birds

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ function draw() {
     // draw sky & clounds
     function drawSky(colour){
       ctx.beginPath();
-      ctx.fillStyle = colour;
-      ctx.fillRect(0,canvas.height - 540,canvas.width,canvas.height - 200);
+      ctx.fillStyle = colour; //set colour to provided hex
+      ctx.fillRect(0,canvas.height - 540,canvas.width,canvas.height - 200); //fill rectangle with colour leave 200px at the bottom for grass
     }
-    drawSky('#e0f2fc');
+    drawSky('#e0f2fc'); //draw sky
     // draw birds
-
+    function drawBird(posX,posY,wingWidth){
+      ctx.beginPath();
+      ctx.moveTo(posX,posY); //go to selected X,Y
+      ctx.lineTo(posX-wingWidth,posY-wingWidth); //left wing, move to x - wing width and go up by wing width
+      ctx.moveTo(posX,posY); //go back to the middle
+      ctx.lineTo(posX+wingWidth,posY-wingWidth) //right wing
+      ctx.stroke();
+    }
+    drawBird(40,40,15);
+    drawBird(700,200,25);
+    drawBird(200,150,17);
     // draw grass
 
     // draw rainbow
